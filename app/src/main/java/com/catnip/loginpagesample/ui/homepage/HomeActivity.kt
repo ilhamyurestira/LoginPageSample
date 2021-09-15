a6b51d25a8fb7fe837902a6e35c6ecb96282fc64
ilhamyurestira <ilhamyurest22@gmail.com>
2021-09-15 17:24:33 +0700

    finishing feature homepage

--- app/src/main/java/com/catnip/loginpagesample/ui/homepage/HomeActivity.kt
+++ app/src/main/java/com/catnip/loginpagesample/ui/homepage/HomeActivity.kt
@@ -1,11 +1,72 @@
 package com.catnip.loginpagesample.ui.homepage
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.catnip.loginpagesample.R
+import com.catnip.loginpagesample.data.preference.UserPreference
+import com.catnip.loginpagesample.databinding.ActivityHomeBinding
+import com.catnip.loginpagesample.ui.login.LoginActivity
+import com.catnip.loginpagesample.utils.Constans
+import com.catnip.loginpagesample.utils.ProtectedMediaChromeClient
+import com.shashank.sony.fancytoastlib.FancyToast
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_home)
+
+        supportActionBar?.title = getString(R.string.text_actionbar_homepage)
+
+        //inflate webview
+        setupWebView()
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun setupWebView() {
+        binding.wvPage.settings.javaScriptEnabled = true
+        binding.wvPage.settings.allowContentAccess = true
+        binding.wvPage.settings.domStorageEnabled = true
+        binding.wvPage.webChromeClient = ProtectedMediaChromeClient()
+        binding.wvPage.loadUrl(Constans.URL_WEB_HOMEPAGE)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_homepage, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_action_logout -> {
+                FancyToast.makeText(
+                    this,
+                    getString(R.string.text_logout_success),
+                    FancyToast.LENGTH_LONG,
+                    FancyToast.SUCCESS,
+                    true
+                ).show()
+
+                deleteSessionData()
+                navigateToLogin()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun navigateToLogin() {
+        val intent = Intent(this, LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
+    }
+
+    private fun deleteSessionData() {
+        UserPreference(this).isUserLoggedIn = false
     }
 }
--- app/src/main/java/com/catnip/loginpagesample/utils/Constans.kt
+++ app/src/main/java/com/catnip/loginpagesample/utils/Constans.kt
@@ -1,6 +1,7 @@
 package com.catnip.loginpagesample.utils
 
 object Constans {
+    const val URL_WEB_HOMEPAGE = "open.spotify.com"
     const val DUMMY_USERNAME = "admin@gmail.com"
     const val DUMMY_PASSWORD = "admin"
 }
